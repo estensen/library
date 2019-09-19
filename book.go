67499e7ac9de13790a2b1982ffe83f85fadbfa14
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-09-19 14:42:35 +0200

    Print all books in db

---
+++ book.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Book struct {
+	isbn string
+	title string
+	author string
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root@/library")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	books := make([]*Book, 0)
+	for rows.Next() {
+		book := new(Book)
+		err := rows.Scan(&book.isbn, &book.title, &book.author)
+		if err != nil {
+			log.Fatal(err)
+		}
+		books = append(books, book)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, book := range books {
+		fmt.Printf("%s, %s, %s", book.title, book.author, book.isbn)
+	}
+
+	fmt.Println("All books printed")
+}
