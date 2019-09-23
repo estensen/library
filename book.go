87ce35f88faf366e66481e274d4313bbd62700d0
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-09-23 08:47:17 +0200

    Log errors

--- book.go
+++ book.go
@@ -27,12 +27,14 @@ func main() {
 
 func booksIndex(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
+		log.Printf("%s on %s is not allowed\n", r.Method, "/books")
 		http.Error(w, http.StatusText(405), 405)
 		return
 	}
 
 	rows, err := db.Query("SELECT isbn, title, author FROM books")
 	if err != nil {
+		log.Println("could not get books: ", err)
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
@@ -43,12 +45,14 @@ func booksIndex(w http.ResponseWriter, r *http.Request) {
 		book := new(Book)
 		err := rows.Scan(&book.Isbn, &book.Title, &book.Author)
 		if err != nil {
+			log.Println("could not scan books: ", err)
 			http.Error(w, http.StatusText(500), 500)
 			return
 		}
 		books = append(books, book)
 	}
 	if err = rows.Err(); err != nil {
+		log.Println("got an error while looping books", err)
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
@@ -58,6 +62,7 @@ func booksIndex(w http.ResponseWriter, r *http.Request) {
 
 func booksShow(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
+		log.Printf("%s on %s is not allowed\n", r.Method, "/books/show")
 		http.Error(w, http.StatusText(405), 405)
 		return
 	}
@@ -86,6 +91,7 @@ func booksShow(w http.ResponseWriter, r *http.Request) {
 
 func booksCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "PUT" {
+		log.Printf("%s on %s is not allowed\n", r.Method, "/books/create")
 		http.Error(w, http.StatusText(405), 405)
 		return
 	}
@@ -94,12 +100,14 @@ func booksCreate(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
 	author := r.FormValue("author")
 	if isbn == "" || title == "" || author == "" {
+		log.Println("cannot create a book without a isbn, title or author")
 		http.Error(w, http.StatusText(400), 400)
 		return
 	}
 
 	_, err := db.Exec("INSERT INTO books VALUES(?, ?, ?)", isbn, title, author)
 	if err != nil {
+		log.Printf("a book could not be inserted into the db: %s", err)
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
@@ -110,6 +118,7 @@ func booksCreate(w http.ResponseWriter, r *http.Request) {
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	data, err := json.MarshalIndent(payload, "", " ")
 	if err != nil {
+		log.Printf("%s could not be marshaled into JSON: %s", data, err)
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
